783f6d8565bcc1334863430da8de2b9feb25b6c6
shudev <syuketa@gmail.com>
2013-05-30 07:55:00 +0900

    no message

--- ScrollingNavigationDemo/ViewController.h
+++ ScrollingNavigationDemo/ViewController.h
@@ -12,3 +12,55 @@
 - (IBAction)previousPage:(id)sender;
 
 @end
+
+/*
+ 
+ http://morishin127.tumblr.com/
+ 
+ ボタンの画像変更などのViewの変更はメソッドが呼ばれると同時にされているわけじゃないみたい。
+ メソッドが呼ばれて直ちに反映されてほしいときは
+[[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.05]];
+
+を使うといいかも。例えば
+
+ [button1 setImage:image1 forState:UIControlStateNormal];
+ [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.05]];
+
+ のように書けばすぐに変更が画面に反映されるようになる。
+ 詳しい仕組みはわからないけど。
+ スマートじゃないかも。応急処置的な。
+*/
+
+
+/*
+ 
+ setNeedsDisplay uiview
+ 
+ */
+
+/*
+ 
+ UIViewをdrawRectが呼ばれない。
+ なんで？
+ ってしらべたら
+ 表示対象のUIViewがhiddennだったり表示エリアになかったり幅0pxだと動かないみたい。
+ 勉強になりました
+ 
+ setNeedsDisplay
+ メソッド
+ で再レンダリングを予約できます
+ 
+ 参考サイト
+ http://msmori.blogspot.jp/2011/01/drawrect.html
+ 
+ 参考サイト
+ http://blog.f60k.com/uiview_drawrect_setneedsdisplay/
+ 
+ */
+
+
+/*
+ 
+ http://blog.f60k.com/uiview_drawrect_setneedsdisplay/*/
+
+
